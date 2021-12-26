74be6988e13dc88266bbe7bc0a30477c3dc3dd83
elelan's mac <elelanv@gmail.com>
2021-12-26 16:47:33 +0530

    init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+// components
+import UseMemoExample from './UseMemoExample'
+import UseContextExample from './UseContextExample'
+import UseCalkbackExample from './UseCalkbackExample'
+import UseRefExample from './UseRefExample'
+
+// styles
+import './style.css'
+
+const App = () => (
+    <>
+    <UseContextExample />
+    </>
+);
+
+export default App;
